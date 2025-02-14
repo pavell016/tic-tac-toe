fa9e7b1e47f05d8813c45a3c8864b63a38441c6c
pavell <pavellupan29@gmail.com>
2025-02-14 11:07:41 +0100

    start building server, and clientForm

---
+++ tic-tac-toe/tic-tac-toe/Form2.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace tic_tac_toe
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 20F);
+            label1.Location = new Point(294, 3);
+            label1.Name = "label1";
+            label1.Size = new Size(213, 46);
+            label1.TabIndex = 1;
+            label1.Text = "TIC-TAC-TOE";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(248, 107);
+            button1.Name = "button1";
+            button1.Size = new Size(70, 64);
+            button1.TabIndex = 2;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(367, 107);
+            button2.Name = "button2";
+            button2.Size = new Size(70, 64);
+            button2.TabIndex = 3;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(485, 107);
+            button3.Name = "button3";
+            button3.Size = new Size(70, 64);
+            button3.TabIndex = 4;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(484, 193);
+            button4.Name = "button4";
+            button4.Size = new Size(70, 64);
+            button4.TabIndex = 7;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(366, 193);
+            button5.Name = "button5";
+            button5.Size = new Size(70, 64);
+            button5.TabIndex = 6;
+            button5.Text = "button5";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(247, 193);
+            button6.Name = "button6";
+            button6.Size = new Size(70, 64);
+            button6.TabIndex = 5;
+            button6.Text = "button6";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(484, 296);
+            button7.Name = "button7";
+            button7.Size = new Size(70, 64);
+            button7.TabIndex = 10;
+            button7.Text = "button7";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(366, 296);
+            button8.Name = "button8";
+            button8.Size = new Size(70, 64);
+            button8.TabIndex = 9;
+            button8.Text = "button8";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(247, 296);
+            button9.Name = "button9";
+            button9.Size = new Size(70, 64);
+            button9.TabIndex = 8;
+            button9.Text = "button9";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button7);
+            Controls.Add(button8);
+            Controls.Add(button9);
+            Controls.Add(button4);
+            Controls.Add(button5);
+            Controls.Add(button6);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Name = "Form2";
+            Text = "Form2";
+            Load += Form2_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+    }
+}
---
+++ tic-tac-toe/tic-tac-toe/Server.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tic_tac_toe
+{
+    internal class Server
+    {
+        private static IPAddress ServerIP;
+        private static int PortIP;
+        private static IPEndPoint ServerEndPoint;
+        static readonly object locker = new object();
+
+    }
+}
